9e30e970b6101d4550332a9570a90ed2c4ba3f3a
tonasun <tona.sun@meetsocial.cn>
2019-12-30 14:17:55 +0800

    新增factory

--- springapp/src/main/java/com/yang/factory/Application.java
+++ springapp/src/main/java/com/yang/factory/Application.java
@@ -1,9 +1,7 @@
 package com.yang.factory;
 
-import com.yang.factory.conf.AopComponentScanConf;
 import com.yang.factory.dao.IUserDao;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.FactoryBean;
+import com.yang.factory.dao.UserDaoImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -17,13 +15,19 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class Application {
     public static void main(String[] args) throws Exception {
-        //BeanFactory是一个用于取容器中对象的工厂
+        /*//BeanFactory是一个用于取容器中对象的工厂
         BeanFactory annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AopComponentScanConf.class);
         IUserDao userDao = (IUserDao) annotationConfigApplicationContext.getBean("myFactoryBean");
         IUserDao userDao2 = (IUserDao) annotationConfigApplicationContext.getBean("myFactoryBean");
         FactoryBean myFactoryBean = (FactoryBean) annotationConfigApplicationContext.getBean("&myFactoryBean");
         //myFactoryBean.getObject()和正常方法调用一样
         System.out.println(userDao.equals(myFactoryBean.getObject()));
-        System.out.println(userDao.equals(userDao2));
+        System.out.println(userDao.equals(userDao2));*/
+
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.register(UserDaoImpl.class);
+        annotationConfigApplicationContext.refresh();
+        IUserDao userDao = (IUserDao) annotationConfigApplicationContext.getBean("userDao");
+        System.out.println(userDao);
     }
 }
--- springapp/src/main/java/com/yang/factory/dao/UserDaoImpl.java
+++ springapp/src/main/java/com/yang/factory/dao/UserDaoImpl.java
@@ -1,9 +1,12 @@
 package com.yang.factory.dao;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @Description: TODO
  * @Author: tona.sun
  * @Date: 2019/12/23 10:30
  */
-public class UserDaoImpl implements IUserDao{
+@Service("userDao")
+public class UserDaoImpl implements IUserDao {
 }
